c01a011e905be1463a3dd998a079cdcace23f155
devsajid <74415469+devsajid@users.noreply.github.com>
2022-04-19 16:35:04 +0600

    ddddddg

---
+++ src/Components/Home/Service/Service.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Service.css";
+const Service = ({ service }) => {
+  const { name, img, description, price } = service;
+  return (
+    <div className="service">
+      <img src={img} alt="Our service" />
+      <h3>{name}</h3>
+      <p>Price:${price}</p>
+      <p>
+        <small>{description}</small>
+      </p>
+      <button className="btn btn-primary">Book {name}</button>
+    </div>
+  );
+};
+
+export default Service;
